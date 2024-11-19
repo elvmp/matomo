f376219841cbfe60b7e249ecd14a9848b6ab5518
caddoo <matthew.caddoo@gmail.com>
2024-11-19 14:48:37 +1300

    Temp modify tests to identify first of month failure

--- plugins/Overlay/tests/UI/Overlay_spec.js
+++ plugins/Overlay/tests/UI/Overlay_spec.js
@@ -183,7 +183,7 @@ describe("Overlay", function () {
         testEnvironment.overrideConfig('General', 'enable_framed_allow_write_admin_token_auth', 1);
         testEnvironment.save();
 
-        await page.goto('?module=Widgetize&action=iframe&disableLink=0&widget=1&moduleToWidgetize=Actions&actionToWidgetize=getPageUrls&idSite=3&period=year&date=today&disableLink=1&widget=1&flat=1&token_auth=a4ca4238a0b923820dcc509a6f75849f', {waitUntil: 'networkidle0'});
+        await page.goto('?module=Widgetize&action=iframe&disableLink=0&widget=1&moduleToWidgetize=Actions&actionToWidgetize=getPageUrls&idSite=3&period=year&date=yesterday&disableLink=1&widget=1&flat=1&token_auth=a4ca4238a0b923820dcc509a6f75849f', {waitUntil: 'networkidle0'});
         await page.waitForNetworkIdle();
 
         const row = await page.jQuery('.dataTable tbody tr:first', { waitFor: true });
@@ -199,6 +199,13 @@ describe("Overlay", function () {
         await popup.waitForTimeout(2500);
 
         await removeOptOutIframe(popup);
+
+        await page.jQuery('#overlayDateRangeSelection').click();
+        const dropDownItem = await page.waitForSelector('#overlayDateRangeSelection ul li:nth-child(1)');
+        dropDownItem.click();
+
+        await page.waitForNetworkIdle();
+
         expect(await popup.screenshot({fullPage: true})).to.matchImage('loaded_from_actions');
     });
 
